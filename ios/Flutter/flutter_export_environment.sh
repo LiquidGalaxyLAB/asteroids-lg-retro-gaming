76dba6be0ebd9c640488cc122c6f28d9cd530a57
leoruas <leo.coelho.ruas@gmail.com>
2021-05-27 21:01:02 -0300

    updated project

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,6 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src_flutter\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\Users\Leo\Desktop\lg_retro_gaming"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
