acbc67880f5b47910acdad42a4eefdc162961abe
leoruas <leo.coelho.ruas@gmail.com>
2021-05-30 13:43:04 -0300

    added socket event listeners

--- server/index.js
+++ server/index.js
@@ -17,12 +17,30 @@ app.get('/:id', (req, res) => {
 //sockets
 io.on("connect", (socket) => {
     console.log(`User connected with id: ${socket.id}!`);
+    //player to players object
     players[socket.id] = {
         pos: {
             x: 0,
             y: 0
         },
     }
+
+    //update player position
+    socket.on("update-player", function(dir){
+        console.log("Update player: ", dir);
+    })
+
+    //open game based on the name given
+    socket.on("open-game", function(game) {
+        console.log("Open game: ", game);
+    })
+
+    //remove from players object when they disconnect
+    socket.on("disconnect", function() {
+        console.log(`Player with id ${socket.id} disconnected!`);
+
+        delete players[socket.id]; 
+    })
 })
 
 const port = 8028;
