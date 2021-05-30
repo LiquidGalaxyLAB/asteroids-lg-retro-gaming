b5aa72e19f207efacc20731c10408a78eb629e83
leoruas <leo.coelho.ruas@gmail.com>
2021-05-30 13:33:31 -0300

    added socketio to server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 var id;
 
 app.use(express.static(__dirname + '/public'))
@@ -12,6 +13,10 @@ app.get('/:id', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
     
+//sockets
+io.on("connect", (socket) => {
+    console.log(`User connected with id: ${socket.id}!`);
+})
 
 const port = 8028;
 http.listen(port, () => {
