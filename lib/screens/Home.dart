599b487e828eef42dd264255338da51885ea6b78
leoruas <leo.coelho.ruas@gmail.com>
2021-06-05 11:28:29 -0300

    connected app to server with sockets

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +9,17 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late Socket socket;
+  late String? ip;
+  late String? port;
+  bool hasConnected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    connectToServer();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,5 +27,31 @@ class _HomeState extends State<Home> {
         child: Text("Liquid Galaxy Retro Gaming"),
       ),
     );
+  }
+
+  void connectToServer() async {
+    await dotenv.load(fileName: ".env");
+    try {
+      //get variables from .env file
+      ip = dotenv.env['SERVER_IP'];
+      port = dotenv.env['SERVER_PORT'];
+      print('ip: $ip port: $port');
+
+      // Configure socket to connect with server ip
+      socket = io('http://$ip:$port', <String, dynamic>{
+        'transports': ['websocket'],
+        'autoConnect': false,
+      });
+
+      socket.connect();
+
+      socket.on(
+          'connect', (_) => print('Connect to socket with id: ${socket.id}'));
+      setState(() {
+        hasConnected = true;
+      });
+    } catch (e) {
+      print(e.toString());
+    }
   }
 }
