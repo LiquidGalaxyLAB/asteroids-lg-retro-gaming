c3fdcdc48e3636239ff0e78dc8f43d53bf2891a8
leoruas <leo.coelho.ruas@gmail.com>
2021-08-18 16:32:48 -0300

    updated app title in main

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Login App',
+      title: 'Liquid Galaxy Retro Gaming',
       initialRoute: '/splash',
       routes: {
         '/splash': (context) => SplashScreen(),
