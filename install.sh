7497e0dc30d552614821d9ae691f291d35c181a0
Letícia Carvalho <letcarvalho.ramos@gmail.com>
2022-08-25 15:53:30 -0300

    feat: improved the installer

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 #Create logs directory if it doesnt exist yet
 mkdir -p ./logs
 
+PW="$1"
+
 #Create file name with name as date
 date=$(date +%m-%d-%y)
 filename="$date.txt"
@@ -22,7 +24,7 @@ DATA=`cat /etc/iptables.conf | grep "tcp" | grep "8111" | grep "3123"`
 if [ "$DATA" == "" ]; then
     time=$(date +%H:%M:%S)
     echo "[$time] Port 3123 not open, opening port..." | tee -a ./logs/$filename
-    sudo sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> ./logs/$filename
+    echo $PW | sudo -S sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> ./logs/$filename
 else
     time=$(date +%H:%M:%S)
     echo "[$time] Port already open." | tee -a ./logs/$filename
@@ -36,17 +38,17 @@ npm install 2>> ../logs/$filename
 cd ..
 
 # Add access for pm2
-sudo chown lg:lg /home/lg/.pm2/rpc.sock /home/lg/.pm2/pub.sock
+echo $PW | sudo -S chown lg:lg /home/lg/.pm2/rpc.sock /home/lg/.pm2/pub.sock
 
 # Stop server if already started
-pm2 delete LGRG_PORT:3123 2> /dev/null
+echo $PW | sudo -S pm2 delete LGRG_PORT:3123 2> /dev/null
 
 # Start server
 time=$(date +%H:%M:%S)
 echo "[$time] Starting pm2..." | tee -a ./logs/$filename
-pm2 start ./server/index.js --name LGRG_PORT:3123 2>> ./logs/$filename
+echo $PW | sudo -S pm2 start ./server/index.js --name LGRG_PORT:3123 2>> ./logs/$filename
 
-pm2 save 2>> ./logs/$filename
+echo $PW | sudo -S pm2 save 2>> ./logs/$filename
 
 # Add automatic pm2 resurrect script
 time=$(date +%H:%M:%S)
@@ -57,9 +59,4 @@ eval $RESURRECT 2>> ./logs/$filename
 time=$(date +%H:%M:%S)
 echo "[$time] Installation complete. Reboot machine to finish installation" | tee -a ./logs/$filename
 
-read -p "Do you want to reboot your machine now? [Y/n]: " yes
-
-if [[ $yes =~ ^[Yy]$ ]]
-then
-  reboot
-fi
+echo $PW | sudo -S reboot
