77609c6d30a9e485ec6510d9512533626dd145fc
Letícia Carvalho <letcarvalho.ramos@gmail.com>
2022-08-27 00:30:54 -0300

    fix(install.sh): fixed some permissions

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#Create logs directory if it doesnt exist yet
-mkdir -p /home/lg/lg-retro-gaming/logs
-
 PW="$1"
+
+BASE_FOLDER="/home/lg/lg-retro-gaming"
 
 #Create file name with name as date
 date=$(date +%Y-%m-%d)
@@ -11,7 +10,7 @@ filename="$date.txt"
 
 # Add to log with timestamp
 time=$(date +%H:%M:%S)
-echo "[$time] Installing Liquid Galaxy Retro Gaming..." | tee -a /home/lg/lg-retro-gaming/logs/$filename
+echo "[$time] Installing Liquid Galaxy Retro Gaming..."
 
 # Open port 3123
 
@@ -23,18 +22,18 @@ DATA=`cat /etc/iptables.conf | grep "tcp" | grep "8111" | grep "3123"`
 
 if [ "$DATA" == "" ]; then
     time=$(date +%H:%M:%S)
-    echo "[$time] Port 3123 not open, opening port..." | tee -a /home/lg/lg-retro-gaming/logs/$filename
-    echo $PW | sudo -S sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> /home/lg/lg-retro-gaming/logs/$filename
+    echo "[$time] Port 3123 not open, opening port..."
+    echo $PW | sudo -S sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf
 else
     time=$(date +%H:%M:%S)
-    echo "[$time] Port already open." | tee -a /home/lg/lg-retro-gaming/logs/$filename
+    echo "[$time] Port already open."
 fi
 
 # Install dependencies on server folder
 time=$(date +%H:%M:%S)
-echo "[$time] Installing dependencies..." | tee -a /home/lg/lg-retro-gaming/logs/$filename
-cd /home/lg/lg-retro-gaming/server
-npm install 2>> ./home/lg/lg-retro-gaming/logs/$filename
+echo "[$time] Installing dependencies..."
+cd $BASE_FOLDER/server
+echo $PW | sudo -S npm install -y
 cd ..
 
 # Add access for pm2
@@ -45,16 +44,16 @@ echo $PW | sudo -S pm2 delete LGRG_PORT:3123 2> /dev/null
 
 # Start server
 time=$(date +%H:%M:%S)
-echo "[$time] Starting pm2..." | tee -a /home/lg/lg-retro-gaming/logs/$filename
-echo $PW | sudo -S pm2 start ./server/index.js --name LGRG_PORT:3123 2>> /home/lg/lg-retro-gaming/logs/$filename
+echo "[$time] Starting pm2..."
+echo $PW | sudo -S pm2 start ./server/index.js --name LGRG_PORT:3123
 
-echo $PW | sudo -S pm2 save 2>> /home/lg/lg-retro-gaming/logs/$filename
+echo $PW | sudo -S pm2 save
 
 # Add automatic pm2 resurrect script
 time=$(date +%H:%M:%S)
-echo "[$time] Updating resurrect script..." | tee -a /home/lg/lg-retro-gaming/logs/$filename
+echo "[$time] Updating resurrect script..."
 RESURRECT=$(pm2 startup | grep 'sudo')
-eval $RESURRECT 2>> /home/lg/lg-retro-gaming/logs/$filename
+echo $PW | sudo -S eval $RESURRECT
 
 time=$(date +%H:%M:%S)
-echo "[$time] Installation complete. Reboot machine to finish installation" | tee -a /home/lg/lg-retro-gaming/logs/$filename
+echo "[$time] Installation complete. Reboot machine to finish installation"
